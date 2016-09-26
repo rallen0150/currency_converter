f073faeedca66f53438172448a9c8d1edf3099ea
rallen0150 <rallen0150@gmail.com>
2016-09-26 17:04:38 -0400

    second commit

--- currency_converter.py
+++ currency_converter.py
@@ -1,8 +1,8 @@
 # Need a dictionary to store all the wanted conversion rates.
 # Rates are all towards converting per USD.
 conversion_rate = {"USD": 1, "EUR": .89, "BTC": .0016, "JPY": 100.3}
-money_c = 0
 
+# Got help with the equation to convert from Tommy.
 def conversion(self, other):
     other.amount = conversion_rate[self.currency]/conversion_rate[other.currency]*other.amount
     return other.amount
@@ -13,15 +13,13 @@ class Money:
         self.amount = amount
         self.currency = currency
 
-
     def __add__(self, other):
-        # Got help with the equation to convert from Tommy.
         conversion(self, other)
-        return self.amount + other.amount
+        return Money(self.amount + other.amount, self.currency)
 
     def __sub__(self, other):
         conversion(self, other)
-        return self.amount - other.amount
+        return Money(self.amount - other.amount, self.currency)
 
     def __ge__(self, other):
         conversion(self, other)
@@ -41,7 +39,7 @@ class Money:
 
     def __mul__(self, other):
         conversion(self, other)
-        return self.amount * other.amount
+        return Money(self.amount * other.amount, self.currency)
 
     def __eq__(self, other):
         conversion(self, other)
@@ -51,7 +49,10 @@ class Money:
         conversion(self, other)
         return self.amount != other.amount
 
+    def __str__(self):
+        return "{} {}".format(self.amount, self.currency)
+
 # Prints out the Math functions of the converting rates.
-print("{0:.2f}".format(Money(10, "USD") + Money(10, "EUR")))
+print(Money(100, "USD") + Money(56.32, "EUR") + Money(1.2, "BTC") + Money(8, "USD"))
 # Prints out the Boolean functions of the converting rates.
 print(Money(10, "USD") >= Money(10, "EUR"))
